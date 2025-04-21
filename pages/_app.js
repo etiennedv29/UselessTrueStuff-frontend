d76b35dc4e021b3d5b43f8c8f68672cda1e663e0
Etienne de V <edevalmont@gmail.com>
2025-04-21 17:53:55 +0200

    add google ads

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,9 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Next.js App</title>
+        <title>Useless True Stuff</title>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8211374496683215"
+     crossorigin="anonymous"></script>
       </Head>
       <Component {...pageProps} />
     </>
