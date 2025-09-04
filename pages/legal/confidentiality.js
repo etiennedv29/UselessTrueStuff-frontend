29255bc663b502432391f68a5f8d8186316bcae5
Etienne de V <edevalmont@gmail.com>
2025-09-04 12:19:56 +0200

    confidentialité change pour soft delete

--- pages/legal/confidentiality.js
+++ pages/legal/confidentiality.js
@@ -7,6 +7,7 @@ function confidentialityPage() {
 <p className = {styles.legalText}>Nous collectons des données personnelles lors de la création d’un compte (email, pseudonyme, mot de passe hashé).</p>
 <p className = {styles.legalText}>Les données ne sont pas revendues. Elles peuvent être utilisées pour vous contacter ou sécuriser l’accès à votre compte.</p>
 <p className = {styles.legalText}>Vous pouvez exercer vos droits (accès, rectification, suppression) en nous écrivant à contact@uts.fr.</p>
+<p className = {styles.legalText}>A la suppression de votre compte, les données personnelles sont supprimées, les données publiques sont anonymisées et conservées</p>
 
     </div>
   )
